0f6001f8dd4f87673bb5f3c3682c1c696a680280
JavohirDevloper <muhammadiyevj72@gmail.com>
2024-01-22 22:20:14 +0500

    fixsed

--- src/modules/elons/_api.js
+++ src/modules/elons/_api.js
@@ -15,7 +15,6 @@ const { LikedElons } = require("./liked-elons");
 let router = express.Router();
 
 let AddElonM = [isloggedIn, hasRole(["user"])];
-let AllElonsM = [isloggedIn, hasRole(["admin", "super_admin"])];
 let UpdateElonsAllM = [isloggedIn, hasRole(["amin", "super_admin"])];
 let UpdateElonsM = [isloggedIn, hasRole(["user"])];
 let DeleteElonsM = [isloggedIn, hasRole(["user"])];
@@ -25,7 +24,7 @@ let LikedElonsM = [isloggedIn, hasRole([isloggedIn, hasRole("user")])];
 let FindByIdElonsM = [isloggedIn, hasRole("user", "admin", "super_admin")];
 
 router.post("/elons", AddElonM, add_elons);
-router.get("/elons", AllElonsM, all_elons);
+router.get("/elons", all_elons);
 router.put("/elonsadmin/:id", UpdateElonsAllM, update_all_elons);
 router.put("/elons/:id", UpdateElonsM, update_elons);
 router.delete("/elons/:id", DeleteElonsM, delete_elons);
