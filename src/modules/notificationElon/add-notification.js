741bf55be388322e3bb569438a22a6e10ddcfd13
Ayubxon0811 <ayubxonrahimov98@gmail.com>
2024-02-06 16:23:18 +0500

    adding validate user elon

--- src/modules/notificationElon/add-notification.js
+++ src/modules/notificationElon/add-notification.js
@@ -1,13 +1,23 @@
+const { NotFoundError } = require("../../shared/errors");
+const User = require("../users/User");
 const { NotficationElonModel } = require("./NotificationElon");
 
 const AddNotifiocation = async ({ body, user }) => {
   let { text, elon_id, user_id } = body;
+  let userElons = await User.findOne({
+    user_id,
+    $and: [{ elons: { $elemMent: elon_id } }],
+  });
+  if (!userElons) {
+    throw new NotFoundError("bu elon siz tanlagan userni eloni emas :(");
+  }
   let createnotification = await NotficationElonModel.create({
     text,
     elon: elon_id,
     notfication_user: user_id,
     notificationCreator: user._id,
   });
+
   return { message: "notification create", data: createnotification };
 };
 
